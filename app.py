94d18f25728f3ea8270ef3a196cfbed596f6c494
Devesh Mehta <devesh1217@gmail.com>
2023-11-08 12:30:00 +0530

    7 commit

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask,request
+from flask_mail import Mail, Message
 app=Flask(__name__)
 
 @app.route('/')
 def api():
     return 'devesh mehta'
+
+# Configure Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587  # Use the appropriate port for your email provider
+app.config['MAIL_USE_TLS'] = True  # Use TLS for security
+app.config['MAIL_USE_SSL'] = False  # Don't use SSL if you're using TLS
+app.config['MAIL_USERNAME'] = 'u22cs035@coed.svnit.ac.in'
+app.config['MAIL_PASSWORD'] = 'medphquozgeiijlt'
+
+mail = Mail(app)
+
+def send_email(subject, recipients, text_body, html_body=None):
+    msg = Message(subject, sender='u22cs035@coed.svnit.ac.in', recipients=recipients)
+    msg.body = text_body
+    if html_body:
+        msg.html = html_body
+    mail.send(msg)
+
+@app.route('/send_emails')
+def send_emails():
+    data=request.get_json();
+    recipients = [data]
+    subject = 'Dear '+data['name']+', Thanks to conatact.'
+    text_body = 'This is a test email from Flask-Mail.'
+    html_body = '<p>Dear '+data['name']+', Thanks to conatact.<br><h1>Devesh</h1></p>'
+    
+    send_email(subject, recipients, text_body, html_body)
+    
+    return 
+
 
 if __name__ == '__main__':
     app.run()
